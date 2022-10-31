2937a73cceb60bf7f7d9bf63d0e818bbad0cf72d
Aaron Jimenez <55520679+MadKingAaron@users.noreply.github.com>
2022-10-30 18:29:22 -0700

    Wrote ShellSort in Python

---
+++ PythonAlgorithms/ShellSort.py
@@ -0,0 +1,13 @@
+from math import log
+import InsertionSort
+def ShellSort(lst: list) -> int:
+    count = 0
+    passes = int(log(len(lst))/log(2))
+    while passes >= 1:
+        increment = int(2 ** (passes - 1))
+        for i in range(increment):
+            count += InsertionSort.insertionSortModified(lst, i, increment)
+        
+        count -= 1
+    
+    return count
