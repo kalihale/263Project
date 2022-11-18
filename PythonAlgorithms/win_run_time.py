5b204395c3e4619957570e523e9c5ed0ac6601d1
Aaron Jimenez <55520679+MadKingAaron@users.noreply.github.com>
2022-11-18 14:56:11 -0800

    Update gitignore

--- PythonAlgorithms/win_run_time.py
+++ PythonAlgorithms/win_run_time.py
@@ -3,7 +3,7 @@ import os
 algo_files = ['BubbleSort.py', 'InsertionSort.py', 'MergeQuickSort.py', 'RadixSort.py', 'ShellSort.py']
 
 def run_tests(samples = 10):
-    prefix = "D:/Users/rocea/anaconda3/envs/pytorch/python.exe d:/Users/rocea/Desktop/Project/263Project/PythonAlgorithms/"
+    prefix = "D:/Users/rocea/anaconda3/envs/py_profiler/python.exe d:/Users/rocea/Desktop/Project/263Project/PythonAlgorithms/"
 
     for i in range(0, samples):
         for algo in algo_files:
