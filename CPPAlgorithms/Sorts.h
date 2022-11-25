b3d46080e20c64ae5cd2e66eb0fcd302a030ef87
Kali <kalihale@outlook.com>
2022-11-25 15:04:45 -0800

    C++ test updates

--- CPPAlgorithms/Sorts.h
+++ CPPAlgorithms/Sorts.h
@@ -4,33 +4,34 @@
 
 #ifndef INC_263PROJECT_SORTS_H
 #define INC_263PROJECT_SORTS_H
+
 #include <string>
 #include <cmath>
 #include <random>
 #include "DoubleLinkedList.h"
 
-template <typename T>
+template<typename T>
 
 class Sorts {
 public:
     struct Node {
         T data;
-        struct Node* next = nullptr;
+        struct Node *next = nullptr;
     };
 
-    T* list;
-    Node* buckets;
+    T *list;
+    Node *buckets;
 
     class GenericCombineBuckets {
     public:
-        T* list;
-        Node* buckets;
+        T *list;
+        Node *buckets;
+
         // ／(^ㅅ^)＼ buckets is an array of starting nodes for linked lists
         //           list is the full array of elements
         //           listlen is the length of the list
         //           numBuckets is the number of linkedlists in buckets
-        void combineBuckets(T* list, Node* buckets, int listlen, int numBuckets)
-        {
+        void combineBuckets(T *list, Node *buckets, int listlen, int numBuckets) {
             this->list = list;
             this->buckets = buckets;
             int j = 0;
@@ -43,7 +44,8 @@ public:
             }
         }
     };
-    static int bubbleSort(int* list, int listLen) {
+
+    static int bubbleSort(int *list, int listLen) {
         int count = 0;
         int numberOfPairs = listLen;
         bool swappedElements = true;
@@ -53,7 +55,7 @@ public:
             swappedElements = false;
             for(int i = 0; i < numberOfPairs; ++i) {
                 count++;
-                if(list[i] > list[i + 1]){
+                if(list[i] > list[i + 1]) {
                     temp = list[i];
                     list[i] = list[i + 1];
                     list[i + 1] = temp;
@@ -64,16 +66,14 @@ public:
         return count;
     };
 
-    static int insertionSort(int* list, int listLen) {
+    static int insertionSort(int *list, int listLen) {
         int count = 0;
         int n = listLen;
-        for (int i = 1; i < n; i++)
-        {
+        for(int i = 1; i < n; i++) {
             int newElement = list[i];
             int location = i - 1;
             count++;
-            while (location >= 0 && list[location] > newElement)
-            {
+            while(location >= 0 && list[location] > newElement) {
                 list[location + 1] = list[location];
                 location = location - 1;
                 count++;
@@ -83,19 +83,17 @@ public:
         return count;
     };
 
-    static int insertionSortModified(int* list, int listLen, int start, int increment) {
+    static int insertionSortModified(int *list, int listLen, int start, int increment) {
         int count = 0;
         int n = listLen;
         //  <@  Must have a loop which compares each number in the sublist to each previous number in its sublist
         //      (starts at i + increment and ends when => n)
-        for(int j = start + increment; j < n; j += increment)
-        {
+        for(int j = start + increment; j < n; j += increment) {
             //  <@  Inside this is where the insertion sort logic goes
             int newElement = list[j];
             int location = j - increment;
             count++;
-            while (location >= 0 && list[location] > newElement && location + increment < listLen)
-            {
+            while(location >= 0 && list[location] > newElement && location + increment < listLen) {
                 list[location + increment] = list[location];
                 location = location - increment;
                 count++;
@@ -104,14 +102,13 @@ public:
         }
         return count;
     };
-    static int shellSort(int* list, int listLen) {
+
+    static int shellSort(int *list, int listLen) {
         int count = 0;
-        int passes = (int)(std::log(listLen)/std::log(2));
-        while(passes >= 1)
-        {
+        int passes = (int) (std::log(listLen) / std::log(2));
+        while(passes >= 1) {
             int increment = std::pow(2, passes - 1);
-            for(int i = 0; i < increment; i++)
-            {
+            for(int i = 0; i < increment; i++) {
                 count += insertionSortModified(list, listLen, i, increment);
             }
             passes--;
@@ -119,13 +116,12 @@ public:
         return count;
     };
 
-    static int radixSort(int* list, int listLen, int keySize) {
+    static int radixSort(int *list, int listLen, int keySize) {
         //  <@  Create buckets
-        DoubleLinkedList<int>* buckets = new DoubleLinkedList<int>[10];
-        auto* gcb = new GenericCombineBuckets();
+        DoubleLinkedList<int> *buckets = new DoubleLinkedList<int>[10];
+        auto *gcb = new GenericCombineBuckets();
         //  <@  Initialize linkedlists in array "buckets"
-        for(int i = 0; i < 10; i++)
-        {
+        for(int i = 0; i < 10; i++) {
             buckets[i] = *new DoubleLinkedList<int>();
         }
         //  <@  Initialize shift at 1
@@ -133,11 +129,9 @@ public:
         //  <@  Up until the largest digit (starting with the ones digit for all numbers)
         //      Note: Once the number of digits in shift is larger than the number of digits in a number,
         //      (list[j] / shift) % 10 will return 0, which will put the number in the 0 bucket
-        for(int i = 1; i <= keySize; i++)
-        {
+        for(int i = 1; i <= keySize; i++) {
             //  <@  For the entire list, sort the digit in question into buckets 0-9
-            for(int j = 0; j < listLen; j++)
-            {
+            for(int j = 0; j < listLen; j++) {
                 buckets[(list[j] / shift) % 10].add(list[j]);
             }
             //  <@  Combine the buckets (using a generic class)
@@ -148,71 +142,58 @@ public:
         return 1;
     };
 
-    static int mergeSort(int* list, int listLen, int first, int last) {
+    static int mergeSort(int *list, int listLen, int first, int last) {
         int middle;
         int count = 0;
-        if(first < last)
-        {
-            middle = (first + last)/2;
+        if(first < last) {
+            middle = (first + last) / 2;
             count += mergeSort(list, listLen, first, middle);
-            count += mergeSort(list, listLen, middle+1, last);
-            count += mergeLists(list, listLen, first, middle, middle+1, last);
+            count += mergeSort(list, listLen, middle + 1, last);
+            count += mergeLists(list, listLen, first, middle, middle + 1, last);
         }
         return count;
     };
 
-    static int mergeLists(int* list, int listLen, int start1, int end1, int start2, int end2) {
+    static int mergeLists(int *list, int listLen, int start1, int end1, int start2, int end2) {
         int count = 0;
-        int* result = new int[listLen];
+        int *result = new int[listLen];
         int finalStart = start1;
         int finalEnd = end2;
         int indexC = 0;
-        while(start1 <= end1 && start2 <= end2)
-        {
+        while(start1 <= end1 && start2 <= end2) {
             count++;
-            if(list[start1] < list[start2])
-            {
+            if(list[start1] < list[start2]) {
                 result[indexC] = list[start1];
                 start1++;
-            }
-            else
-            {
+            } else {
                 result[indexC] = list[start2];
                 start2++;
             }
             indexC++;
         }
-        if(start1 <= end1)
-        {
-            for(int i = start1; i <= end1; i++)
-            {
+        if(start1 <= end1) {
+            for(int i = start1; i <= end1; i++) {
                 result[indexC] = list[i];
                 indexC++;
             }
-        }
-        else
-        {
-            for(int i = start2; i <= end2; i++)
-            {
+        } else {
+            for(int i = start2; i <= end2; i++) {
                 result[indexC] = list[i];
                 indexC++;
             }
         }
         indexC = 0;
-        for(int i = finalStart; i <= finalEnd; i++)
-        {
+        for(int i = finalStart; i <= finalEnd; i++) {
             list[i] = result[indexC];
             indexC++;
         }
         return count;
     };
 
-    static int quickSortPivotFirst(int* list, int first, int last, int* comparisons)
-    {
+    static int quickSortPivotFirst(int *list, int first, int last, int *comparisons) {
         int count = 0;
         int pivot;
-        if(first < last)
-        {
+        if(first < last) {
             pivot = Sorts::pivot(first, list, first, last, comparisons);
             quickSortPivotFirst(list, first, pivot - 1, comparisons);
             quickSortPivotFirst(list, pivot + 1, last, comparisons);
@@ -220,12 +201,10 @@ public:
         return count;
     };
 
-    static int quickSortPivotLast(int* list, int first, int last, int* comparisons)
-    {
+    static int quickSortPivotLast(int *list, int first, int last, int *comparisons) {
         int count = 0;
         int pivot;
-        if(first < last)
-        {
+        if(first < last) {
             pivot = Sorts::pivot(last, list, first, last, comparisons);
             quickSortPivotLast(list, first, pivot - 1, comparisons);
             quickSortPivotLast(list, pivot + 1, last, comparisons);
@@ -233,7 +212,7 @@ public:
         return count;
     };
 
-    static int quickSortPivotRandom(int* list, int first, int last, int* comparisons) {
+    static int quickSortPivotRandom(int *list, int first, int last, int *comparisons) {
         std::random_device rd;
         std::mt19937 mt(rd());
         std::uniform_int_distribution<int> dist(first, last);
@@ -247,18 +226,15 @@ public:
         return count;
     };
 
-    static int pivot(int pivot, int* list, int left, int right, int* comparisons)
-    {
+    static int pivot(int pivot, int *list, int left, int right, int *comparisons) {
         int temp = list[right];
         list[right] = list[pivot];
         list[pivot] = temp;
         int i = left;
 
-        for(int j = i; j < right; j++)
-        {
+        for(int j = i; j < right; j++) {
             comparisons[0]++;
-            if(list[j] < list[right])
-            {
+            if(list[j] < list[right]) {
                 temp = list[i];
                 list[i] = list[j];
                 list[j] = temp;
--- CPPAlgorithms/main.cpp
+++ CPPAlgorithms/main.cpp
@@ -4,33 +4,121 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <chrono>
+#include <sys/time.h>
+#include <ctime>
 #include "Sorts.h"
 
-int main(int argc, const char * argv[]) {
+using std::cout; using std::endl;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::system_clock;
+
+int main(int argc, const char *argv[]) {
+    const int RUNS = 1000;
     int arr[1000];
-    // ／(^ㅅ^)＼ TODO: Find junk data
-    for(int & i : arr) {
-        i = rand() % 1000;
+    int *comparisons = new int[1];
+
+    long totalTime;
+    long startTime;
+    long endTime;
+
+    // ／(^ㅅ^)＼ BubbleSort Tests
+    totalTime = 0;
+    for(int i = 0; i < RUNS; ++i) {
+        for(int &j: arr) {
+            j = rand() % 1000;
+        }
+        comparisons[0] = 0;
+
+        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        Sorts<int>::bubbleSort(arr, 1000);
+        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        totalTime += endTime - startTime;
     }
 
-    int* comparisons = new int[1];
+    std::cout << "BubbleSort Average Time: " << (double)totalTime / RUNS << std::endl;
 
-    comparisons[0] = 0;
+//    // ／(^ㅅ^)＼ Insertion Sort Tests
+//    totalTime = 0;
+//    for(int i = 0; i < RUNS; ++i) {
+//        for(int &j: arr) {
+//            j = rand() % 1000;
+//        }
+//        comparisons[0] = 0;
+//
+//        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        Sorts<int>::insertionSort(arr, 1000);
+//        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        totalTime += endTime - startTime;
+//    }
+//
+//    std::cout << "InsertionSort Average Time: " << (double)totalTime / RUNS << std::endl;
 
-    // ／(^ㅅ^)＼ Working:
-//    Sorts<int>::bubbleSort(arr, 1000);
-//    Sorts<int>::insertionSort(arr, 1000);
-//    Sorts<int>::shellSort(arr, 1000);
-//    Sorts<int>::mergeSort(arr, 1000, 0, 1000);
-//    Sorts<int>::quickSortPivotFirst(arr, 0, 1000, comparisons);
-//    Sorts<int>::quickSortPivotLast(arr, 0, 1000, comparisons);
-    Sorts<int>::quickSortPivotRandom(arr, 0, 1000, comparisons);
+//    // ／(^ㅅ^)＼ ShellSort Times
+//    totalTime = 0;
+//    for(int i = 0; i < RUNS; ++i) {
+//        for(int &j: arr) {
+//            j = rand() % 1000;
+//        }
+//        comparisons[0] = 0;
+//
+//        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        Sorts<int>::shellSort(arr, 1000);
+//        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        totalTime += endTime - startTime;
+//    }
+//
+//    std::cout << "ShellSort Average Time: " << (double)totalTime / RUNS << std::endl;
 
-//    Sorts<int>::radixSort(arr, 1000, 3);
+//    // ／(^ㅅ^)＼ RadixSort Times
+//    totalTime = 0;
+//    for(int i = 0; i < RUNS; ++i) {
+//        for(int &j: arr) {
+//            j = rand() % 1000;
+//        }
+//        comparisons[0] = 0;
+//
+//        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        Sorts<int>::radixSort(arr, 1000);
+//        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        totalTime += endTime - startTime;
+//    }
+//
+//    std::cout << "RadixSort Average Time: " << (double)totalTime / RUNS << std::endl;
 
-    std::cout << comparisons[0] << std::endl;
+//    // ／(^ㅅ^)＼ MergeSort Times
+//    totalTime = 0;
+//    for(int i = 0; i < RUNS; ++i) {
+//        for(int &j: arr) {
+//            j = rand() % 1000;
+//        }
+//        comparisons[0] = 0;
+//
+//        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        Sorts<int>::mergeSort(arr, 1000, 0, 1000);
+//        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        totalTime += endTime - startTime;
+//    }
+//
+//    std::cout << "MergeSort Average Time: " << (double)totalTime / RUNS << std::endl;
 
-    for(int i : arr) {
-        std::cout << i << std::endl;
-    }
+//    // ／(^ㅅ^)＼ QuickSort Times
+//    totalTime = 0;
+//    for(int i = 0; i < RUNS; ++i) {
+//        for(int &j: arr) {
+//            j = rand() % 1000;
+//        }
+//        comparisons[0] = 0;
+//
+//        startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        Sorts<int>::quickSortPivotFirst(arr, 0, 1000, comparisons);
+//        endTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+//        totalTime += endTime - startTime;
+//    }
+//
+//    std::cout << "QuickSort Average Time: " << (double)totalTime / RUNS << std::endl;
+
+    delete[] comparisons;
 }
